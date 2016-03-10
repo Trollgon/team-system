f6d3ae01fb291828b86a3ca962c8725d8495b788
Trollgon <trollgon@ac-whispers.com>
2016-03-11 00:26:19 +0100

    Simple Application - does nothing

---
+++ files/lib/page/IndexPage.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace tourneysystem\page;
+use wcf\page\AbstractPage;
+
+/**
+ * Shows the index page.
+ *
+ * @author	Tim Düsterhus
+ * @copyright	2011 - 2012 Tim Düsterhus
+ * @license	GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl-3.0.txt>
+ * @package	de.trollgon.tourneysystem
+ */
+class IndexPage extends AbstractPage { }
